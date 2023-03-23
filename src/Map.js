20f12ac703763ace7f37a1a4d121354eacee41de
Anuma Thomas <tombrownanuma@gmail.com>
2023-03-23 23:23:17 +0100

    Latest

--- src/Map.js
+++ src/Map.js
@@ -4,7 +4,6 @@ import {CiSearch} from 'react-icons/ci';
 import geoJson from "./geojson.json";
 import NavBar from "./components/NavBar";
 import Popup from "./components/Popup";
-import 'mapbox-gl/dist/mapbox-gl.css'
 
 const weatherApi = {
     key: "8d8a86c8e525d8cb00d8c2850dfb03fd",
